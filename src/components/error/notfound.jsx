55d395c3b255fc7454ad5e5ca9110ca2204ef82a
Tran Vinh Thuy <thuy.tranvinh@vn.bosch.com>
2019-03-29 19:51:41 +0700

    finishing

---
+++ src/components/error/notfound.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+const notFound = () => {
+  return <div>Component is not found</div>;
+};
+
+export default notFound;
--- src/containers/Course/Course.js
+++ src/containers/Course/Course.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
 
 class Course extends Component {
-    render () {
-        return (
-            <div>
-                <h1>_COURSE_TITLE_</h1>
-                <p>You selected the Course with ID: _ID_</p>
-            </div>
-        );
+  render() {
+    console.log(this.props);
+
+    let title = new URLSearchParams(this.props.location.search);
+
+    console.log(title.entries());
+    for (const item of title.entries()) {
+      title = item[1];
+      console.log(item);
     }
+
+    return (
+      <div>
+        <h1>{title}</h1>
+        <p>You selected the Course with ID: {this.props.match.params.id}</p>
+      </div>
+    );
+  }
 }
 
 export default Course;
